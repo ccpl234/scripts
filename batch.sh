cf6f035d7e59b30d5037da2d369f6a420098b2c0
ccpl234 <35296334+ccpl234@users.noreply.github.com>
2018-01-10 21:46:23 +1000

    Create batch.sh

---
+++ batch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt-get install git build-essential cmake libuv1-dev libmicrohttpd-dev
+sudo git clone https://github.com/xmrig/xmrig.git
+cd xmrig
+mkdir build
+cd build
+sudo cmake ..
+sudo make
+sudo sysctl -w vm.nr_hugepages=128
+sudo ./xmrig -o us-east.cryptonight-hub.miningpoolhub.com:20580 -t 16 -u ccpl.ccpl -p password
