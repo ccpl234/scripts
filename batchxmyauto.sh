7f2950579b4f938e31270e78b4e1fd175ca542ca
ccpl234 <35296334+ccpl234@users.noreply.github.com>
2018-01-29 15:41:32 +1000

    Create batchxmyauto.sh

---
+++ batchxmyauto.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt-get -y update
+sudo apt-get -y install git build-essential cmake libuv1-dev libmicrohttpd-dev
+sudo git clone https://github.com/xmrig/xmrig.git
+cd xmrig
+mkdir build
+cd build
+sudo cmake ..
+sudo make
+sudo sysctl -w vm.nr_hugepages=128
+sudo ./xmrig/build/xmrig -o us-east.cryptonight-hub.miningpoolhub.com:20580 -t 16 -u ccpl.scripttest -p password --donate-level 1
