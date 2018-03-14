1455f174f14b5e552f0eebeaa899eb6cef2be379
ccpl234 <35296334+ccpl234@users.noreply.github.com>
2018-03-14 15:59:48 +1000

    Update batchxmyauto.sh

--- batchxmyauto.sh
+++ batchxmyauto.sh
@@ -3,5 +3,5 @@ sudo apt-get -y update
 sudo apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake 
 sudo git clone https://github.com/JayDDee/cpuminer-opt.git 
 cd cpuminer-opt 
-sudo chmod +x ./build.sh 
-sudo ./build.sh 
+sudo chmod +x ./build-allarch.sh 
+sudo ./build-allarch.sh 
