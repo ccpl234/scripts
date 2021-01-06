443b43fdea36d5d5fa563e73508aed394893d8a6
ccpl234 <35296334+ccpl234@users.noreply.github.com>
2021-01-06 15:39:10 +1000

    Update batchautoeth.sh

--- batchautoeth.sh
+++ batchautoeth.sh
@@ -4,8 +4,8 @@ sudo dpkg -i /tmp/${CUDA_REPO_PKG}
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub 
 rm -f /tmp/${CUDA_REPO_PKG}
 sudo apt-get -y update
-sudo apt-get install cuda-drivers
+sudo apt-get -y install cuda-drivers
 mkdir eth
 cd eth
-sudo wget https://github.com/develsoftware/GMinerRelease/releases/download/2.40/gminer_2_40_linux64.tar.xz
-sudo tar -xvf gminer_2_40_linux64.tar.xz
+sudo wget https://github.com/trexminer/T-Rex/releases/download/0.19.5/t-rex-0.19.5-linux-cuda10.0.tar.gz
+sudo tar -xvf t-rex-0.19.5-linux-cuda10.0.tar.gz
