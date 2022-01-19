9e8c5077c67dea9a9612b0ebc51d109c7c34254a
ccpl234 <35296334+ccpl234@users.noreply.github.com>
2022-01-19 13:35:57 +1000

    Update batchautoeth.sh

--- batchautoeth.sh
+++ batchautoeth.sh
@@ -1,11 +1,6 @@
-CUDA_REPO_PKG=cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
-wget -O /tmp/${CUDA_REPO_PKG} https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/${CUDA_REPO_PKG} 
-sudo dpkg -i /tmp/${CUDA_REPO_PKG}
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub 
-rm -f /tmp/${CUDA_REPO_PKG}
-sudo apt-get -y update
-sudo apt-get -y install cuda-drivers
+sudo apt install -y nvidia-drivers-470
+sudo nvidia-smi
 mkdir eth
 cd eth
-sudo wget http://trex-miner.com/download/t-rex-0.20.3-linux.tar.gz
-sudo tar -xvf t-rex-0.20.3-linux.tar.gz
+sudo wget https://github.com/trexminer/T-Rex/releases/download/0.24.8/t-rex-0.24.8-linux.tar.gz
+sudo tar -xvf t-rex-0.24.8-linux.tar.gz
